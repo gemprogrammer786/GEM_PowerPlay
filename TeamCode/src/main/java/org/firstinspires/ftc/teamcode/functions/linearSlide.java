f4756753289db2d1aed71947babd123a92764dc1
Pawan <pawang.edu@gmail.com>
2022-09-25 18:02:17 -0500

    Added  dependencies {      implementation 'org.openftc:easyopencv:1.5.2'   }

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/functions/linearSlide.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.functions;
+
+public class linearSlide {
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/touchSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/touchSensor.java
@@ -9,45 +9,45 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-public class touchSensor extends LinearOpMode{
+public class touchSensor extends LinearOpMode {
 
-    TouchSensor touchSensor_front = hardwareMap.get(TouchSensor.class,"touchSensor_front");
+    TouchSensor touchSensor_front = hardwareMap.get(TouchSensor.class, "touchSensor_front");
 
     @Override
     public void runOpMode() {
 
-    waitForStart();
+        waitForStart();
 
         while (opModeIsActive()) {
-        if (touchSensor_front.isPressed()) {
-            telemetry.addData("Path: ","Touch Sensor is Pressed");
-            telemetry.update();
+            if (touchSensor_front.isPressed()) {
+                telemetry.addData("Path: ", "Touch Sensor is Pressed");
+                telemetry.update();
 
-            // Send telemetry message to signify robot waiting;
-            telemetry.addData("Status", "Ready to run");    //
-            telemetry.update();
+                // Send telemetry message to signify robot waiting;
+                telemetry.addData("Status", "Ready to run");    //
+                telemetry.update();
 
-            // Wait for the game to start (driver presses PLAY)
-            waitForStart();
+                // Wait for the game to start (driver presses PLAY)
+                waitForStart();
 
-            while (opModeIsActive()) {
-                if (touchSensor_front.isPressed()) {
-                    telemetry.addData("Path: ","Touch Sensor is Pressed");
-                    telemetry.update();
+                while (opModeIsActive()) {
+                    if (touchSensor_front.isPressed()) {
+                        telemetry.addData("Path: ", "Touch Sensor is Pressed");
+                        telemetry.update();
 
+                    } else {
+                        telemetry.addData("Path: ", "Waiting for it to be pressed");
+                        telemetry.update();
+                    }
                 }
-                else {
-                    telemetry.addData("Path: ","Waiting for it to be pressed");
-                    telemetry.update();
-                }
+                telemetry.addData("Path", "Complete");
+                telemetry.update();
+                sleep(1000);
+
             }
-            telemetry.addData("Path", "Complete");
-            telemetry.update();
-            sleep(1000);
+
 
         }
 
-
     }
-
 }
